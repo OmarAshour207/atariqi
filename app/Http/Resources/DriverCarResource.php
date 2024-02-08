40673801abd33c99cdf35154875f1e8f7c0f351a
Omar Ashour <omarashour207@gmail.com>
2024-02-08 17:45:39 +0200

    update the path

--- app/Http/Resources/DriverCarResource.php
+++ app/Http/Resources/DriverCarResource.php
@@ -15,13 +15,13 @@ class DriverCarResource extends JsonResource
             'driver-id'     => $this->{"driver-id"},
             'driver-type'   => new DriverTypeResource($this->driverType),
             'car_form_img'  => Storage::url("uploads/$driverId/$this->car_form_img"),
-            'license_img'   => public_path("storage/uploads/$driverId/$this->license_img"),
-            'car_front_img' => public_path("storage/uploads/$driverId/$this->car_front_img"),
-            'car_back_img'  => public_path("storage/uploads/$driverId/$this->car_back_img"),
-            'car_rside_img' => public_path("storage/uploads/$driverId/$this->car_rside_img"),
-            'car_lside_img' => public_path("storage/uploads/$driverId/$this->car_lside_img"),
-            'car_insideFront_img'  => public_path("storage/uploads/$driverId/$this->car_insideFront_img"),
-            'car_insideBack_img'  => public_path("storage/uploads/$driverId/$this->car_insideBack_img"),
+            'license_img'   => Storage::url("uploads/$driverId/$this->license_img"),
+            'car_front_img' => Storage::url("uploads/$driverId/$this->car_front_img"),
+            'car_back_img'  => Storage::url("uploads/$driverId/$this->car_back_img"),
+            'car_rside_img' => Storage::url("uploads/$driverId/$this->car_rside_img"),
+            'car_lside_img' => Storage::url("uploads/$driverId/$this->car_lside_img"),
+            'car_insideFront_img'  => Storage::url("uploads/$driverId/$this->car_insideFront_img"),
+            'car_insideBack_img'  => Storage::url("uploads/$driverId/$this->car_insideBack_img"),
             'date_of_add'  => $this->date_of_add
         ];
     }
