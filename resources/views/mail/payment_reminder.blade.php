dd6e1ba34b8396aa949caf33ba6cadb94ead817b
Omar Ashour <omarashour207@gmail.com>
2024-07-20 10:44:02 +0200

    fix text align

--- resources/views/mail/payment_reminder.blade.php
+++ resources/views/mail/payment_reminder.blade.php
@@ -10,9 +10,9 @@
 
 <body>
 
-<h1>اشعار تذكير بدفع المستحقات</h1>
+<h1 style="text-align: right">اشعار تذكير بدفع المستحقات</h1>
 
-<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2; text-align: right">
     <div style="margin:50px auto;width:70%;padding:20px 0">
         <div style="border-bottom:1px solid #eee">
             <a href="https://atariqi.com" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">عطريقي</a>
@@ -22,7 +22,7 @@
         <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">
             {{ $details['amount'] . ' ' . 'ريال سعودي' }}
         </h2>
-        <p style="font-size:0.9em;">Regards,<br />عطريقي</p>
+        <p style="font-size:0.9em;">مع تحياتي,<br />عطريقي</p>
         <hr style="border:none;border-top:1px solid #eee" />
     </div>
 </div>
