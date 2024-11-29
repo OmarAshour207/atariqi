edcf48de4dea77ae236d17acd4ff8158af7f18e0
Omar Ashour <omarashour207@gmail.com>
2024-11-30 00:37:39 +0200

    add source lat and lng

--- app/Http/Resources/Driver/SugWeeklyDriverResource.php
+++ app/Http/Resources/Driver/SugWeeklyDriverResource.php
@@ -10,11 +10,16 @@ class SugWeeklyDriverResource extends JsonResource
 {
     public function toArray($request)
     {
+        $roadWay = $this->booking->{"road-way"};
         return [
             'id'            => $this->id,
             'action'        => $this->action,
             'date-of-add'   => $this->{"date-of-add"},
             'viewed'        => $this->viewed,
+            'destination_lat' => $roadWay == 'from' ? $this->booking->lat : $this->booking->university->lat,
+            'destination_lng' => $roadWay == 'from' ? $this->booking->lng : $this->booking->university->lng,
+            'source_lat'    => $roadWay == 'from' ? $this->booking->university->lat : $this->booking->lat,
+            'source_lng'    => $roadWay == 'from' ? $this->booking->university->lng : $this->booking->lng,
 //            'passenger'     => new UserSampleResource($this->whenLoaded('passenger')),
             'trip'          => new WeekRideBookingResource($this->booking),
             'delivery_info' => $this->whenLoaded('deliveryInfo'),
--- app/Http/Resources/SuggestionDriver.php
+++ app/Http/Resources/SuggestionDriver.php
@@ -8,11 +8,17 @@ class SuggestionDriver extends JsonResource
 {
     public function toArray($request)
     {
+        $roadWay = $this->booking->{"road-way"};
+
         return [
             'id'            => $this->id,
             'action'        => $this->action,
             'date-of-add'   => $this->{"date-of-add"},
             'viewed'        => $this->viewed,
+            'destination_lat' => $roadWay == 'from' ? $this->booking->lat : $this->booking->university->lat,
+            'destination_lng' => $roadWay == 'from' ? $this->booking->lng : $this->booking->university->lng,
+            'source_lat'    => $roadWay == 'from' ? $this->booking->university->lat : $this->booking->lat,
+            'source_lng'    => $roadWay == 'from' ? $this->booking->university->lng : $this->booking->lng,
             'trip'          => new DayRideBookingResource($this->booking),
             'driver'        => new UserSampleResource($this->whenLoaded('driver')),
             'driverinfo'    => new DriverInfoResource($this->whenLoaded('driverinfo')),
