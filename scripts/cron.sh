8f27d0893e08d58ef9de9439ad288306134a49ae
Omar Ashour <omarashour207@gmail.com>
2026-07-03 23:24:19 +0200

    fix

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd /home/u186572384/domains/atariqi.com/public_html && php artisan schedule:run >> /dev/null 2>&1
+cd /home/u186572384/domains/atariqi.com/public_html && php artisan schedule:work >> /dev/null 2>&1
